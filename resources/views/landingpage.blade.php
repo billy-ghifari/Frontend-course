63e3a5090cc02b292f7c942450374527bbab4e31
unknown <rizkiraffi26@gmail.com>
2023-11-14 16:06:27 +0700

    landing page home

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -17,7 +17,7 @@
 <!-- Custom CSS -->
 <link href="dist/css/style.min.css" rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!-- WARNING: Respond.js  doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -28,31 +28,69 @@
 @endsection
 
 @section('content')
-<div class="justify-content-between" style=" background-color:#49BBBD; border-radius:  0px 0px 100px 100px;">
-    <div class="container">
-        <div class="section1 row">
-            <div class="col-6" style="margin-top:7rem">
-                <h1 style="font-size:30px; font-weight:600; color:white; margin-bottom:20px">
-                    <span style="color: black;">Belajar secara online</span> kini menjadi
-                    <br>lebih mudah dengan berbagai
-                    <br>kemudahan yang tersedia.
-                </h1>
-                <span style="font-size: 18px; font-weight:400; color:white;">Tingkatkan pengalaman belajar Anda dengan
-                    <br>DTC, platform yang menarik dan memberikan
-                    <br>pembelajaran secara interaktif.
-                </span>
-                <div class=" row" style="margin-top: 20px;">
-                    <div class="col">
-                        <button class="submit" style="background-color:#80cfd1; border-radius:40px; width:180px; height:60px; border-color:#80cfd1">
-                            <p style="font-weight: 500; color:white; margin-top:14px">Join for free</p>
-                        </button>
+<div class="justify-content-between">
+    <div class="section1" style=" background-color:#49BBBD; border-radius:  0px 0px 100px 100px;">
+        <div class="container">
+            <div class="section1 row">
+                <div class="col-6" style="margin-top:7rem">
+                    <h1 style="font-size:30px; font-weight:600; color:white; margin-bottom:20px">
+                        <span style="color: black;">Belajar secara online</span> kini menjadi
+                        <br>lebih mudah dengan berbagai
+                        <br>kemudahan yang tersedia.
+                    </h1>
+                    <span style="font-size: 18px; font-weight:400; color:white;">Tingkatkan pengalaman belajar Anda dengan
+                        <br>DTC, platform yang menarik dan memberikan
+                        <br>pembelajaran secara interaktif.
+                    </span>
+                    <div class=" row" style="margin-top: 20px;">
+                        <div class="col-4 pr-0">
+                            <a href="" class="btn" style="background-color:#80cfd1; border-radius:40px; width:170px; height:60px;
+                        font-weight: 500; color:white; margin-top:10px; padding-top:17px"> Join for free </a>
+                        </div>
+                        <div class="col-2 pl-3 pr-0">
+                            <div class="card mt-2" style="border-radius: 50%; width:60px; height:60px; background-color:white;">
+                                <i class="fas fa-play" style="color: #23BDEE;  font-size: 21px;padding-top:21px; padding-left:23px;"></i>
+                            </div>
+                        </div>
+                        <div class="col-4 mt-4 mr-5 pl-0">
+                            <span style="color: black;">Watch how it works</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-6" style="margin-top:9rem; margin-bottom:4rem">
-                <img src="/assets/images/unsplash_section1.png" alt="" style="width:610px; height:410px">
+                <div class="col-6" style="margin-top:9rem; margin-bottom:4rem">
+                    <img src="/assets/images/unsplash_section1.png" alt="" style="width:600px; height:400px">
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="section2" style="margin-top: 6rem;">
+        <div class="container" style="text-align: center;">
+            <h1 style="color: #2F327D; font-weight:600">DTC <span style="color:#00CBB8;font-weight:600">online.</span></h1>
+            <p style="color: #696984; margin-top:15px; margin-bottom:3rem">DTC sebagai paket perangkat lunak online yang sangat efisien,
+                <br> menggabungkan semua alat yang diperlukan untuk menjalankan
+                <br> sekolah atau kantor dengan sukses.
+            </p>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 340px; height:380px;">
+                        <div class="card" style=" background-color: #5B72EE;border-radius: 50%; width: 60px; height: 60px;">
+                            <i class="fas fa-file-alt" style="color: white;"></i>
+                        </div>
+                        <div class="card-body" style="text-align: center;">
+                            <p style="color: #2F327D;;">Online Billing,
+                                <br>Invoicing, & Contracts
+                            </p>
+                            <span>Simple and secure control of your
+                                <br>organization’s financial and legal
+                                <br>transactions. Send customized
+                                <br>invoices and contracts</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
--- resources/views/templates/partials/headbar.blade.php
+++ resources/views/templates/partials/headbar.blade.php
@@ -1,30 +1,26 @@
 <header class="topbar fixed-top d-flex header-scrolled" data-navbarbg="skin6" style="background-color:#49BBBD; height:70px; ">
     <div class=" justify-content-between container">
         <div class="row">
-            <div class="col-3" style="margin-top: 1rem;">
+            <div class="col-4" style="margin-top: 25px;">
                 <img src="/assets/images/Union.png" alt="" style="width: 47px; height: 51.6px">
             </div>
-            <div class="col-9" style="margin-top: 1rem;">
+            <div class="col-8" style="margin-top:25px ;">
                 <ul class="nav justify-content-center">
-                    <li class="nav-item">
+                    <li class="nav-item mr-4">
                         <a class="nav-link" href="#" style="color: white;">Home</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mr-4">
                         <a class="nav-link" href="#" style="color: white;">Courses</a>
                     </li>
-                    <li class=" nav-item">
+                    <li class=" nav-item mr-4">
                         <a class="nav-link" href="#" style="color: white;">Blog</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mr-4">
                         <a class="nav-link" href="#" style="color: white;">About Us</a>
                     </li>
-                    <a href="#">
-                        <button class="submit" style="width:100px; height:50px; border-radius:38px; background-color:white; color:black;">
-                            Login</button>
+                    <a href="#" class="btn ml-1" style="width:115px; height:45px; border-radius:38px; background-color:white; color:black; padding-top:9px;">Login
                     </a>
-                    <a href="#">
-                        <button class="submit" style="width:100px; height:50px; border-radius:38px; background-color:#80cfd1; color:white;">
-                            Sign Up</button>
+                    <a href="#" class="btn ml-3" style="width:115px; height:45px; border-radius:38px; background-color:#80cfd1; color:white; padding-top:9px;">Sign Up
                     </a>
                 </ul>
 
