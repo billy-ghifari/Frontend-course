3526d6e39888bff10c108fcd0fcce3b848caeb25
unknown <rizkiraffi26@gmail.com>
2023-11-21 21:33:38 +0700

    course view

--- resources/views/templates/partials/sidebar.blade.php
+++ resources/views/templates/partials/sidebar.blade.php
@@ -1,15 +1,48 @@
-<aside class="left-sidebar" data-sidebarbg="skin6">
-    <div class="scroll-sidebar" data-sidebarbg="skin6">
-        <ul class="sidebar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-book"></i>
-                    <span>Lesson 01 : Introduction about XD</span>
-                </a>
-            </li>
-        </ul>
-        <!-- <a href="" class="btn" style="background-color: #49BBBD; width: 40px; height: 40px;">
-        <i class=" fas fa-long-arrow-alt-left"></i></button></a> -->
-    </div>
+    <aside class="left-sidebar" data-sidebarbg="skin6" style="padding-top: 130px; width:380px;">
+        <!-- Sidebar scroll-->
+        <div class="scroll-sidebar" data-sidebarbg="skin6" style="width: 380px;">
+            <!-- Sidebar navigation-->
+            <nav class="sidebar-nav pt-0 mt-4 ml-2">
+                <ul id="sidebarnav">
+                    <li class="sidebar-item">
+                        <a href="">
+                            <button class="btn text-white" style="height:50px; background-color:#49BBBD; border-radius:10px; font-size:13px; width:365px;">
+                                <div class="row pl-3">
+                                    <i class="fas fa-book" style="padding-top: 3px; padding-left:5px"></i>
+                                    <span style="padding-left: 20px;">Lesson 01 : Introduction about XD</span>
+                                    <span style="padding-left: 50px; color:black">30mins</span>
+                                </div>
+                            </button>
+                        </a>
+                    </li>
 
-</aside>
+                    <li class="sidebar-item">
+                        <a href="">
+                            <button class="btn text-dark mt-3" style="height:50px; background-color:#fcddb4; border-radius:10px; font-size:13px; width:365px;">
+                                <div class="row pl-3">
+                                    <i class="fas fa-book" style="padding-top: 3px; padding-left:5px"></i>
+                                    <span style="padding-left: 20px;">Lesson 01 : Introduction about XD</span>
+                                    <span style="padding-left: 50px; color:black">30mins</span>
+                                </div>
+                            </button>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a href="">
+                            <button class="btn text-white mt-3" style="height:50px; background-color:#e2f0ff; border-radius:10px; font-size:13px; width:365px;">
+                                <div class="row pl-3">
+                                    <i class="fas fa-book text-dark" style="padding-top: 3px; padding-left:5px"></i>
+                                    <span style="padding-left: 20px; color:black">Lesson 01 : Introduction about XD</span>
+                                    <span style="padding-left: 50px; color:black">30mins</span>
+                                </div>
+                            </button>
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+            <!-- End Sidebar navigation -->
+        </div>
+        <!-- End Sidebar scroll-->
+    </aside>
