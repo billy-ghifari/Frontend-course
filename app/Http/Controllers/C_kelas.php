505829d878a94b898457b321de6b2c747e02e9d2
Rafha Arya <wicaksonorafha@gmail.com>
2023-12-15 20:05:57 +0700

    fixing

--- app/Http/Controllers/C_kelas.php
+++ app/Http/Controllers/C_kelas.php
@@ -2,34 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Constants\ApiEndPoint;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class C_Kelas extends Controller
 {
-    private $urlApi;
-
-    public function __construct()
-    {
-        $this->urlApi = env('APP_API'); // Fetch the API URL from the environment variable
-    }
-
     public function index()
     {
-        $token = session('token');
-
-        $response = Http::withToken($token)->get($this->urlApi . ApiEndPoint::$allkelas);
-        $kelas = json_decode($response)->data->data;
-
-        // dd($kelas);
-        // die;
-
-        return view('r_kelas', [
-            'kelas' => $kelas,
-            'urlapi' => $this->urlApi
-        ]);
-        return view('');
+        return view('r_kelas');
     }
 
     public function c_kelas()
