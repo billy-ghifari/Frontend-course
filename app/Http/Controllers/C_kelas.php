ef4c3d49689f66c834e8e6cf9d22993d7e367538
unknown <rizkiraffi26@gmail.com>
2023-12-15 19:59:04 +0700

    blogdetail & kelasdetail

--- app/Http/Controllers/C_kelas.php
+++ app/Http/Controllers/C_kelas.php
@@ -2,34 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Constants\ApiEndPoint;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class C_Kelas extends Controller
 {
-    private $urlApi;
-
-    public function __construct()
+    public function r_kelas()
     {
-        $this->urlApi = env('APP_API'); // Fetch the API URL from the environment variable
-    }
-
-    public function index()
-    {
-        $token = session('token');
-
-        $response = Http::withToken($token)->get($this->urlApi . ApiEndPoint::$allkelas);
-        $kelas = json_decode($response)->data->data;
-
-        // dd($kelas);
-        // dd($kelas);
-
-        return view('crudkelas/r_kelas', [
-            'kelas' => $kelas,
-            'urlapi' => $this->urlApi
-        ]);
-        return view('');
+        return view('crudkelas/r_kelas');
     }
 
     public function c_kelas()
@@ -37,18 +16,8 @@ class C_Kelas extends Controller
         return view('crudkelas/c_kelas');
     }
 
-    public function u_update()
+    public function u_kelas()
     {
-        return view('crudkelas/u_kelas');
-    }
-
-    public function detailkelas()
-    {
-        return view('crudkelas/detailkelas');
-    }
-
-    public function createkelas()
-    {
-        return view('crudkelas/createkelas');
+        return view('crudkelas/r_kelas');
     }
 }
